52649c8a49db38e5abbc6bfb1b3bcca0e62253d0
Namyunsuk <yunsuk0328@naver.com>
2023-12-13 19:45:54 +0900

    feat(Car): 자동차 정보 저장 및 전진, 우승자 반환 기능 구현

---
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -0,0 +1,30 @@
+package racingcar.model
+
+class Car(private val raceCalculation: RaceCalculation) {
+    private val cars = mutableMapOf<String, Int>()
+
+    fun addCars(carNames: List<String>) {
+        for (carName in carNames) {
+            cars[carName] = DEFAULT_FORWARD_COUNT
+        }
+    }
+
+    fun moveForward(carName: String) {
+        val randomNum = raceCalculation.generateRandomNum()
+        if (raceCalculation.calculateForward(randomNum)) {
+            val forwardCount = cars[carName]
+            if (forwardCount != null) {
+                cars[carName] = forwardCount + FORWARD
+            }
+        }
+    }
+
+    fun findWinners(): List<String> {
+        return raceCalculation.calculateWinner(cars)
+    }
+
+    companion object {
+        const val DEFAULT_FORWARD_COUNT = 0
+        const val FORWARD = 1
+    }
+}
